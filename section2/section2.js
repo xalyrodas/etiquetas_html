95cc613bd66b74996056fe3120851a66031527d1
danialuna <dmluna@scl.edu.gt>
2024-05-30 13:23:22 -0600

    section2 Dania

--- section2/section2.js
+++ section2/section2.js
@@ -1,2 +1,33 @@
 let section_2 = document.querySelector(".section_2");
-section_2.innerHTML = `hola soy section2 `;
+section_2.innerHTML = `
+<h2>Texto</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>&lt;html&gt;&lt;/html&gt;</td>
+                    <td>Lenguaje de marcado de hipertexto.</td>
+                </tr>
+                <tr>
+                    <td>&lt;head&gt;&lt;/head&gt;</td>
+                    <td>Sección de encabezado del documento.</td>
+                </tr>
+                <tr>
+                    <td>&lt;body&gt;&lt;/body&gt;</td>
+                    <td>Contenido principal del documento.</td>
+                </tr>
+                <tr>
+                    <td>&lt;p&gt;&lt;/p&gt;</td>
+                    <td>Párrafo.</td>
+                </tr>
+                <tr>
+                    <td>&lt;a href=""&gt;&lt;/a&gt;</td>
+                    <td>Enlace.</td>
+                </tr>
+            </tbody>
+        </table> `;
